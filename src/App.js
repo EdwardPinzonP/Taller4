451a29da1b588989fdbd0eb1a58483304a164edb
EdwardPinzonP <edwardfabianpinzon@gmail.com>
2024-02-05 22:13:03 -0500

    número par o impar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+function DeterminarParImpar() {
+  const [numero, setNumero] = useState('');
+  const [resultado, setResultado] = useState('');
+
+  const definirParImpar = () => {
+    const num = parseInt(numero);
+    if (!isNaN(num)) {
+      if (num % 2 === 0) {
+        setResultado(`${num} es un número par.`);
+      } else {
+        setResultado(`${num} es un número impar.`);
+      }
+    } else {
+      setResultado('Por favor, ingrese un número válido, no se admiten letras ni el campo vacío.');
+    }
+  };
+
+  const reiniciarFormulario = () => {
+    setNumero('');
+    setResultado('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className='text-center'>VALIDADOR DE NÚMEROS PARES O IMPARES</h1>
+      <div className='form-group'>
+      <input className='form-control col-md-12'
+        type="number"
+        placeholder="Ingrese un número"
+        value={numero}
+        onChange={(e) => setNumero(e.target.value)}
+      />
+      </div>
+      <br/>
+      <button className='btn btn-danger col-md-6' type='button' onClick={reiniciarFormulario}>Reiniciar</button>
+      <button className='btn btn-primary col-md-6' type='button' onClick={definirParImpar}>Validar</button>
+      <div className='resultado'>{resultado}</div>
     </div>
   );
 }
 
-export default App;
+export default DeterminarParImpar;
